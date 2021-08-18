c7090dcfdf282af2d62e72440e690cdb3e47e85c
HigorJ <higor_sgomes@outlook.com>
2021-08-18 12:06:42 -0300

    Atividade - Componente curso e listagem de cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { ColecaoCursosComponent } from './colecao-cursos/colecao-cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent,
+    ColecaoCursosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/colecao-cursos/colecao-cursos.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colecao-cursos',
+  templateUrl: './colecao-cursos.component.html',
+  styleUrls: ['./colecao-cursos.component.css']
+})
+export class ColecaoCursosComponent {
+
+}
+
+
